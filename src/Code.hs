ba4582c9a7020a40b5ad173f0a122e198d3606e8
Tom <thomasdhawkins@eaton.com>
2010-05-24 05:25:12 +0000

    C code generated

--- src/Code.hs
+++ src/Code.hs
@@ -7,12 +7,11 @@ import Text.Printf
 import Model
 
 genCode :: [Class] -> IO ()
-genCode classes = putStrLn $ indent $ unlines
-  [ "{"
-  , indent "static uint64_t counter = 0;"
-  , indent $ concatMap codeStateChart $ concat [ s | Class _ s <- classes ]
-  , indent "counter++;"
-  , "}"
+genCode classes = putStrLn $ block $ unlines
+  [ "static uint64_t stateChartGlobalClock = 0;"
+  , ""
+  , concatMap codeStateChart $ concat [ s | Class _ s <- classes ]
+  , "stateChartGlobalClock++;"
   ]
 
 formatId :: String -> String -> String
@@ -22,21 +21,91 @@ formatId prefix id = prefix ++ map f id
   f a = a
 
 sId = formatId "s_"
-cId = formatId "c_"
-tId = formatId "t_"
 
 indent :: String -> String
 indent = unlines . map ("  " ++) . lines
 
+block :: String -> String
+block a = "{\n" ++ indent a ++ "}\n"
+
 codeStateChart :: StateChart -> String
-codeStateChart (StateChart name states connectors transitions) = unlines
-  [ "// " ++ name
-  , "{"
-  , indent $ unlines [ printf "static bool %s = false;" (sId $ stateId state) | state <- states ]
-  , indent $ unlines [ printf "static uint64_t %s_entryTime = 0;" (sId $ stateId state) | state <- states ]
-  , "}"
-  , ""
-  ]
+codeStateChart (StateChart name states transitions) = seq defaultState $ "// " ++ name ++ "\n" ++ block (unlines
+  [ unlines [ printf "static bool %s = false;"           (sId $ stateId state) | state <- states ]
+  , unlines [ printf "static uint64_t %s_entryTime = 0;" (sId $ stateId state) | state <- states ]
+  , concatMap (codeTransition states) transitions
+  ]) ++ "\n"
+  where
+  root = head [ state | state <- states, stateParent state == Nothing ]
+  defaultTransition = case [ t | t <- transitions, transitionSource t == Nothing ] of
+    [t] -> t
+    []  -> error "default state not specified (1)"
+    _   -> error "lower level default transitions not supported"
+  defaultState = if Just (transitionId defaultTransition) /= stateDefaultTrans root
+    then error "default state not specified (2)"
+    else head [ state | state <- states, stateId state == transitionTarget defaultTransition ]
+
+codeTransition :: [State] -> Transition -> String
+codeTransition states t = case transitionSource t of
+  Nothing     -> "// -> " ++ stateName (idState states target) ++ "\n"
+              ++ "if (period20() && !" ++ sId (head b) ++ ") {\n"
+              ++ indent (transAction (transitionAction t) ++ concatMap (stateEntry . idState states) b)
+              ++ "}\n\n"
+    where
+    b = hierarchy states target
+    
+  Just source -> "// " ++ stateName (idState states source) ++ " -> " ++ stateName (idState states target) ++ "\n"
+              ++ "if (period20() && " ++ predicate ++ ") {\n"
+              ++ indent (concatMap (stateExit . idState states) a ++ transAction (transitionAction t) ++ concatMap (stateEntry . idState states) b)
+              ++ "}\n\n"
+    where
+    (a, b) = changedStates states source target
+    predicate = sId target ++ predicateTimeout ++ predicateGuard
+    predicateTimeout = case transitionTimeout t of
+      Nothing -> ""
+      Just i  -> " && (stateChartGlobalClock >= " ++ sId source ++ "_entryTime + (" ++ i ++ "))"
+    predicateGuard = case transitionGuard t of
+      Nothing -> ""
+      Just g  -> " && (" ++ g ++ ")"
+  where
+  target = transitionTarget t
+
+stateExit :: State -> String
+stateExit s = case stateExitAction s of
+  Nothing ->              stateUpdate
+  Just a  -> a ++ "\n" ++ stateUpdate
+  where
+  stateUpdate = sId (stateId s) ++ " = false;\n"
+
+stateEntry :: State -> String
+stateEntry s = case stateEntryAction s of
+  Nothing ->              stateUpdate
+  Just a  -> a ++ "\n" ++ stateUpdate
+  where
+  stateUpdate = sId (stateId s) ++ " = true;\n" ++ sId (stateId s) ++ "_entryTime = stateChartGlobalClock;\n"
+
+transAction :: Maybe String -> String
+transAction Nothing = ""
+transAction (Just a) = a ++ "\n"
+
+idState :: [State] -> Id -> State
+idState states id = head [ s | s <- states, stateId s == id ]
+
+-- (statesExiting, statesEntering)  with proper order.
+changedStates :: [State] -> Id -> Id -> ([Id], [Id])
+changedStates states a b = (reverse a', b')
+  where
+  (a', b') = stripCommonPrefix (hierarchy states a) (hierarchy states b)
+
+hierarchy :: [State] -> Id -> [Id]
+hierarchy states id = case stateParent $ idState states id of
+  Nothing -> [id]
+  Just a  -> hierarchy states a ++ [id]
+
+stripCommonPrefix :: Eq a => [a] -> [a] -> ([a], [a])
+stripCommonPrefix (a : as) (b : bs)
+  | a == b    = stripCommonPrefix as bs
+  | otherwise = (a : as, b : bs)
+stripCommonPrefix a b = (a, b)
 
 {-
 data Class = Class Name [StateChart] deriving Show
@@ -47,4 +116,18 @@ data State = State
   { stateName         :: Name
   , stateId           :: Id
   , stateParent       :: Maybe Id
+  , stateDefaultTrans :: Maybe Id
+  , stateEntryAction  :: Maybe Code
+  , stateExitAction   :: Maybe Code
+  } deriving Show
+
+data Transition = Transition
+  { transitionName    :: Name
+  , transitionTimeout :: Maybe Int
+  , transitionGuard   :: Maybe Code
+  , transitionAction  :: Maybe Code
+  , transitionSource  :: Maybe Id
+  , transitionTarget  :: Id
+  } deriving Show
+
 -}
--- src/Model.hs
+++ src/Model.hs
@@ -2,8 +2,9 @@ module Model
   ( Class         (..)
   , StateChart    (..)
   , State         (..)
-  , Connector     (..)
-  , ConnectorType (..)
+  , Transition    (..)
+  -- , Connector     (..)
+  -- , ConnectorType (..)
   , Name
   , Id
   , Code
@@ -20,7 +21,7 @@ type Code = String
 
 data Class = Class Name [StateChart] deriving Show
 
-data StateChart = StateChart Name [State] [Connector] [Transition] deriving Show
+data StateChart = StateChart Name [State] [Transition] deriving Show
 
 data State = State
   { stateName         :: Name
@@ -29,9 +30,9 @@ data State = State
   , stateDefaultTrans :: Maybe Id
   , stateEntryAction  :: Maybe Code
   , stateExitAction   :: Maybe Code
-  , stateAnd          :: Bool
   } deriving Show
 
+{-
 data Connector = Connector
   { connectorName   :: Name
   , connectorId     :: Id
@@ -39,11 +40,13 @@ data Connector = Connector
   , connectorType   :: ConnectorType
   } deriving Show
 
-data ConnectorType = Junction | Condition | Fork | Join deriving Show
+data ConnectorType = Junction | Condition deriving Show
+-}
 
 data Transition = Transition
   { transitionName    :: Name
-  , transitionTimeout :: Maybe Int
+  , transitionId      :: Id
+  , transitionTimeout :: Maybe Code
   , transitionGuard   :: Maybe Code
   , transitionAction  :: Maybe Code
   , transitionSource  :: Maybe Id
@@ -57,23 +60,21 @@ parseClass :: Record -> Class
 parseClass a = Class (head $ values "_name" a) $ map parseStateChart $ containerRecords "StateCharts" a
 
 parseStateChart :: Record -> StateChart
-parseStateChart a = StateChart
+parseStateChart a = if not $ null $ containerRecords "Connectors" a then error "connectors not supported" else StateChart
   (value "_name" a)
   (map parseState      $ containerRecords "States"      a)
-  (map parseConnector  $ containerRecords "Connectors"  a)
   (map parseTransition $ containerRecords "Transitions" a)
 
 parseState :: Record -> State
-parseState a = State
+parseState a = if values "_stateType" a == ["And"] then error $ "and state not supported: " ++ value "_name" a else State
   { stateName         = value "_name"         a
   , stateId           = ident "_id"           a
   , stateParent       = maybeList $ ident "_parent"       a
   , stateDefaultTrans = maybeList $ ident "_defaultTrans" a
   , stateEntryAction  = maybeList $ body  "_entryAction"  a
   , stateExitAction   = maybeList $ body  "_exitAction"   a
-  , stateAnd          = case values "_stateType" a of { ["And"] -> True; _ -> False }
-  }
-
+  } 
+{-
 parseConnector :: Record -> Connector
 parseConnector a = Connector
   { connectorName   = value "_name"   a
@@ -82,16 +83,18 @@ parseConnector a = Connector
   , connectorType   = case value "_connectorType" a of
       "Junction"  -> Junction
       "Condition" -> Condition
-      "Fork"      -> Fork
-      "Join"      -> Join
+      --"Fork"      -> Fork
+      --"Join"      -> Join
       a           -> error $ "unknown connector type: " ++ a
   }
+-}
 
 parseTransition :: Record -> Transition
 parseTransition a = Transition
   { transitionName    = value "_name" a
+  , transitionId      = ident "_id"   a
   , transitionTimeout = timeout
-  , transitionGuard   = maybeList $ body "_itsGuard"   label
+  , transitionGuard   = maybeList $ if guard == "else" then error "else guards not supported" else guard
   , transitionAction  = maybeList $ body "_itsAction"  label
   , transitionSource  = maybeList $ ident "_itsSource" a
   , transitionTarget  = ident "_itsTarget" a
@@ -99,9 +102,11 @@ parseTransition a = Transition
   where
   label = head $ records "_itsLabel" a
   timeout = case body "_itsTrigger" label of
-    a | isPrefixOf "tm(" a -> Just $ read $ drop 3 $ init a
+    a | isPrefixOf "tm(" a -> Just $ drop 3 $ init a
       | null a             -> Nothing
       | otherwise          -> error $ "not supported non tm() events: " ++ a
+  guard = body "_itsGuard" label
+
 
 maybeList :: [a] -> Maybe [a]
 maybeList a = if null a then Nothing else Just a
