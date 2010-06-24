5ab01916c7bf4d0b4f13e77107ee4cb350c92c9f
Tom <thomasdhawkins@eaton.com>
2010-06-24 06:01:43 -0500

    changed code gen to one function per class, removed periods

--- src/Statechart.hs
+++ src/Statechart.hs
@@ -9,9 +9,20 @@ import Rhapsody
 main :: IO ()
 main = do
   args <- getArgs
-  case args of
-    name : period : args@(_:_) -> readFile (last args) >>= genCode name (read period) (init args) . parseModel . parseRhapsody
-    _ -> help
+  case parseArgs args of
+    Nothing -> help
+    Just (o, inc, sbs) -> readFile sbs >>= genCode o inc . parseModel . parseRhapsody
+
+parseArgs :: [String] -> Maybe (String, [String], String)
+parseArgs a = case a of
+  [a] | head a /= '-' -> Just (takeWhile (/= '.') a ++ ".c", [], a)
+  "-o" : file : rest -> do
+    (_, inc, sbs) <- parseArgs rest
+    Just (file, inc, sbs)
+  "-i" : file : rest -> do
+    (o, inc, sbs) <- parseArgs rest
+    Just (o, file : inc, sbs)
+  _ -> Nothing
 
 help :: IO ()
 help = putStrLn $ unlines
@@ -20,10 +31,21 @@ help = putStrLn $ unlines
   , "  statechart - statechart code generation"
   , ""
   , "SYNOPSIS"
-  , "  statechart <name> <period>{ <include-files> } <rhapsody-file>"
+  , "  statechart [ -o <output-file> | { -i <header-file> } ] <rhapsody-package-file>"
   , ""
   , "DESCRIPTION"
-  , "  Compiles a statechart to <name>.c"
+  , "  Compiles Rhapsody statecharts to C.  Each Rhapsody class will generate a function"
+  , "  that computes the next transition for each of its associated statecharts."
+  , ""
+  , "OPTIONS"
+  , "  -o <name>"
+  , "    Output name.  Creates both <name>.c and <name>.h."
+  , ""
+  , "  -i <file>"
+  , "    Header file to include in the generated code."
+  , ""
+  , "LIMITATIONS"
+  , "  TODO, enough to say, a bunch."
   , ""
   ]
 
